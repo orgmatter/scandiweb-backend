9b9b6097d50206a5ba7dd815c7b11925fdd985d2
orgmatter <orgmatter@gmail.com>
2023-03-28 07:17:34 +0100

    modified headers on our server

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ require "vendor/autoload.php";
 
 header('Access-Control-Allow-Origin: *');
 
+if(Request::method() === "OPTIONS") {
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS');
+}
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS');
 
 header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Origin, Authorization');
