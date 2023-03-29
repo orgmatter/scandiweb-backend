63ffea48f859a787037bb00113d80dc0eee1fe9e
orgmatter <orgmatter@gmail.com>
2023-03-29 02:28:04 +0100

    added htaccess file

--- src/App/Database/Migrations/create-product-schema.php
+++ src/App/Database/Migrations/create-product-schema.php
@@ -4,17 +4,18 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 require '../../../../vendor/autoload.php';
 
-require '../config.php';
+$rootPath = $_SERVER['DOCUMENT_ROOT'];
+require_once($rootPath.'/src/App/Database/config.php');
 
-// Capsule::schema()->create('products', function ($table) {
-//     $table->increments('id');
-//     $table->string('name');
-//     $table->string('price');
-//     $table->string('sku')->unique();
-//     $table->string('productType');
-//     $table->json('attributes');
-//     $table->timestamps();
-//     $table->softDeletes();
-// });
+Capsule::schema()->create('products', function ($table) {
+    $table->increments('id');
+    $table->string('name');
+    $table->string('price');
+    $table->string('sku')->unique();
+    $table->string('productType');
+    $table->json('attributes');
+    $table->timestamps();
+    $table->softDeletes();
+});
 
-echo "you dey mad!";
+echo "migration complete!";
