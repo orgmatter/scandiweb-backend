6b5aa31457fe7582ab278b05c194f72a5e9da78c
orgmatter <orgmatter@gmail.com>
2023-03-29 01:51:05 +0100

    added htaccess file

--- src/App/Routes/Traits/RequestHandlers.php
+++ src/App/Routes/Traits/RequestHandlers.php
@@ -15,8 +15,8 @@ trait RequestHandlers {
     public function getProducts() 
     { 
         try {
-            // $products = $this->productService()->getProducts();
-            return $this->productService()->toJson(["status" => "check"]);
+            $products = $this->productService()->getProducts();
+            return $this->productService()->toJson($products);
         
         }catch(Exception $ex) {
             echo $ex->getMessage();
