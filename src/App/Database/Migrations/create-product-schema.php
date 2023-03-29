b1ee74595b976c74aea3861ae854c014c118cb7a
orgmatter <orgmatter@gmail.com>
2023-03-29 02:24:10 +0100

    added htaccess file

--- src/App/Database/Migrations/create-product-schema.php
+++ src/App/Database/Migrations/create-product-schema.php
@@ -6,13 +6,15 @@ require '../../../../vendor/autoload.php';
 
 require '../config.php';
 
-Capsule::schema()->create('products', function ($table) {
-    $table->increments('id');
-    $table->string('name');
-    $table->string('price');
-    $table->string('sku')->unique();
-    $table->string('productType');
-    $table->json('attributes');
-    $table->timestamps();
-    $table->softDeletes();
-});
+// Capsule::schema()->create('products', function ($table) {
+//     $table->increments('id');
+//     $table->string('name');
+//     $table->string('price');
+//     $table->string('sku')->unique();
+//     $table->string('productType');
+//     $table->json('attributes');
+//     $table->timestamps();
+//     $table->softDeletes();
+// });
+
+echo "you dey mad!";
