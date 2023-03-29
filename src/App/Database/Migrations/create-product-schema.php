713640d78adc6dfaf08d0bab3f8ffd8bad0ace13
orgmatter <orgmatter@gmail.com>
2023-03-29 02:36:42 +0100

    added htaccess file

--- src/App/Database/Migrations/create-product-schema.php
+++ src/App/Database/Migrations/create-product-schema.php
@@ -8,15 +8,15 @@ require_once($rootPath.'/vendor/autoload.php');
 
 require_once($rootPath.'/src/App/Database/config.php');
 
-Capsule::schema()->create('products', function ($table) {
-    $table->increments('id');
-    $table->string('name');
-    $table->string('price');
-    $table->string('sku')->unique();
-    $table->string('productType');
-    $table->json('attributes');
-    $table->timestamps();
-    $table->softDeletes();
-});
+// Capsule::schema()->create('products', function ($table) {
+//     $table->increments('id');
+//     $table->string('name');
+//     $table->string('price');
+//     $table->string('sku')->unique();
+//     $table->string('productType');
+//     $table->json('attributes');
+//     $table->timestamps();
+//     $table->softDeletes();
+// });
 
-echo "migration complete!";
+echo Capsule::class;
