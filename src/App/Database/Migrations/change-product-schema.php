5e0ab80bfe4d553abcc1e342995795ef7d7296e7
orgmatter <orgmatter@gmail.com>
2023-03-29 05:27:41 +0100

    testing my model services

--- src/App/Database/Migrations/change-product-schema.php
+++ src/App/Database/Migrations/change-product-schema.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-require '../../../../vendor/autoload.php';
+$rootPath = $_SERVER['DOCUMENT_ROOT'];
 
-require '../config.php';
+require_once($rootPath.'/vendor/autoload.php');
+
+require_once($rootPath.'/src/App/Database/config.php');
 
 Capsule::schema()->table('products', function ($table) {
     $table->string('productType')->after('sku');
