1351aab5eb01f93b3db5bdebfd0a5940aafc907f
orgmatter <orgmatter@gmail.com>
2023-03-29 02:32:53 +0100

    added htaccess file

--- src/App/Database/Migrations/create-product-schema.php
+++ src/App/Database/Migrations/create-product-schema.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-require '../../../../vendor/autoload.php';
-
 $rootPath = $_SERVER['DOCUMENT_ROOT'];
+
+require_once($rootPath.'/vendor/autoload.php');
+
 require_once($rootPath.'/src/App/Database/config.php');
 
 Capsule::schema()->create('products', function ($table) {
