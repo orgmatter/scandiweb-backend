d9f71be6327b8093216153fe0106e82025239347
orgmatter <orgmatter@gmail.com>
2023-04-04 22:34:02 +0100

    worked on OOP feedback

--- src/App/Products/Product.php
+++ src/App/Products/Product.php
@@ -4,5 +4,10 @@ namespace App\Products;
 
 class Product
 {
-    use App\Routes\Traits\RequestHandlers;
+    // use App\Routes\Traits\RequestHandlers;
+
+    public function shout()
+    {
+        echo "shout";
+    }
 }
