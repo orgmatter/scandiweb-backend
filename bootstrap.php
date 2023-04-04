9e8c944ec6267cde549febdee2fa06be0ca7aa20
orgmatter <orgmatter@gmail.com>
2023-04-04 14:26:46 +0100

    worked on OOP feedback

--- bootstrap.php
+++ bootstrap.php
@@ -5,11 +5,11 @@ use App\Routes\Router;
 $rootPath = $_SERVER["DOCUMENT_ROOT"];
 $routePath = "{$rootPath}/src/App/Routes/routes.php";
 
+echo $routePath;
+
 $app = [
     "router" => new Router,
     "load" => Router::load($routePath),
 ];
-
-echo $routePath;
 
 return $app;
