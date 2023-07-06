e56d7b44ba52e75fdec5fb267ebf2cb38deb7e75
orgmatter <orgmatter@gmail.com>
2023-07-06 10:48:48 +0100

    code refactoring

--- bootstrap.php
+++ bootstrap.php
@@ -6,11 +6,11 @@ $rootPath = $_SERVER["DOCUMENT_ROOT"];
 $routePath = "{$rootPath}/src/App/Routes/routes.php";
 
 
-$app = [
-    "router" => new Router,
-    // "load" => Router::load($routePath),
-];
+// $app = [
+//     "router" => new Router,
+//     "load" => Router::load($routePath),
+// ];
 
-echo var_dump($app);
+echo var_dump(new Router);
 
 return $app;
