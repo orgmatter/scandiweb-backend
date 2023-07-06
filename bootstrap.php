a33acfa952e2265f1ab790ec4810ab9aa20c4669
orgmatter <orgmatter@gmail.com>
2023-07-06 10:44:22 +0100

    code refactoring

--- bootstrap.php
+++ bootstrap.php
@@ -5,10 +5,12 @@ use App\Routes\Router;
 $rootPath = $_SERVER["DOCUMENT_ROOT"];
 $routePath = "{$rootPath}/src/App/Routes/routes.php";
 
-echo "your head again";
+
 $app = [
-    "router" => new Router,
-    "load" => Router::load($routePath),
+    "router" => "new Router",
+    "load" => "Router",
 ];
 
-return $app;
+echo var_dump($app);
+
+// return $app;
